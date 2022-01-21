961556c4d47de573875cb90dff5805f0193c5dc2
Ykhoo <89803794+YukoHo0513@users.noreply.github.com>
2022-01-21 14:48:14 -0800

    eventlistener to the keyboard is added

--- app.js
+++ app.js
@@ -1,50 +1,64 @@
-const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
-const btnReset = document.querySelector('.btn__reset');
-const overlay = document.getElementById('overlay');
-const ul = document.querySelector('ul');
-let missedGuess = 0;
 
-const phrases = ['I do not like cheese', 'Happy new year', 'Sloths move fast', 'Baked pork chop on rice', 'Rainbow on a rainy day' ];
+document.addEventListener('DOMContentLoaded', () => {
+    const qwerty = document.getElementById('qwerty');
+    const phrase = document.getElementById('phrase');
+    const btnReset = document.querySelector('.btn__reset');
+    const overlay = document.getElementById('overlay');
+    const ul = document.querySelector('ul');
+    let missedGuess = 0;
+    const ol = document.querySelector('ol');
 
+    const phrases = ['I do not like cheese', 'Happy new year', 'Sloths move fast', 'Baked pork chop on rice', 'Rainbow on a rainy day' ];
 
-btnReset.addEventListener('click', () => {
-    overlay.style.display = 'none';
-});
+    btnReset.addEventListener('click', () => {
+        overlay.style.display = 'none';
+    });
 
-function getRandomPhraseAsArray(arr) {
-    const randomNum = Math.floor(Math.random() * arr.length);
-    const arrItem = arr[randomNum];
-    return arrItem;
-}
-const arrPhrase = getRandomPhraseAsArray(phrases);
+    function getRandomPhraseAsArray(arr) {
+        const randomNum = Math.floor(Math.random() * arr.length);
+        const arrItem = arr[randomNum];
+        return arrItem;
+    }
+    const arrPhrase = getRandomPhraseAsArray(phrases);
 
-function addPhraseToDisplay(arrItem) {
-    for (let i = 0; i < arrItem.length; i++) {
-        const li = document.createElement('li');
-        li.textContent = arrItem[i];
-        ul.appendChild(li);
-        if (arrItem[i] !== ' ') {
-            li.className = 'letter';
-        } else {
-            li.className = 'space';
+    function addPhraseToDisplay(arrItem) {
+        for (let i = 0; i < arrItem.length; i++) {
+            const li = document.createElement('li');
+            li.textContent = arrItem[i];
+            ul.appendChild(li);
+            if (arrItem[i] !== ' ') {
+                li.className = 'letter';
+            } else {
+                li.className = 'space';
+            }
         }
     }
-}
-addPhraseToDisplay(arrPhrase);
+    addPhraseToDisplay(arrPhrase);
 
-
-const keyQBtn = document.querySelector('.keyQ');
-
-function checkLetter(button) {
-    const lis = ul.childNodes;
-    let match = null;
-    for (let i = 0; i < lis.length; i++) {
-        if (button.textContent === lis[i].textContent) {
-            lis[i].className = 'show';
-            match = button.textContent;
-        } 
+    function checkLetter(button) {
+        const lis = ul.childNodes;
+        let match = null;
+        for (let i = 0; i < lis.length; i++) {
+            if (button.textContent === lis[i].textContent) {
+                lis[i].className = 'show';
+                match = button.textContent;
+            } 
+        }
+        return match;
     }
-    return match;
-}
-console.log(checkLetter(keyQBtn));
+
+    qwerty.addEventListener('click', (e) => {
+        if (e.target.tagName === 'BUTTON') {
+            const button = e.target;
+            if (button.className !== 'chosen') {
+                button.className = 'chosen';
+                const checkingLetter = checkLetter(button);
+                if (checkingLetter === null) {
+                    const li = ol.children[0];
+                    ol.removeChild(li);
+                    missedGuess += 1;
+                }
+            }
+        }
+    });
+});
