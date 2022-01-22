0398f4706de406cfee228f3c5ff520a9d68cbcb1
Ykhoo <89803794+YukoHo0513@users.noreply.github.com>
2022-01-21 19:20:29 -0800

    checkWin function added

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const qwerty = document.getElementById('qwerty');
-    const phrase = document.getElementById('phrase');
     const btnReset = document.querySelector('.btn__reset');
     const overlay = document.getElementById('overlay');
     const ul = document.querySelector('ul');
     let missedGuess = 0;
     const ol = document.querySelector('ol');
+    const h2 = document.querySelector('.title');
 
     const phrases = ['I do not like cheese', 'Happy new year', 'Sloths move fast', 'Baked pork chop on rice', 'Rainbow on a rainy day' ];
 
@@ -24,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function addPhraseToDisplay(arrItem) {
         for (let i = 0; i < arrItem.length; i++) {
             const li = document.createElement('li');
-            li.textContent = arrItem[i];
+            li.textContent = arrItem[i].toLowerCase();
             ul.appendChild(li);
             if (arrItem[i] !== ' ') {
                 li.className = 'letter';
@@ -40,25 +39,48 @@ document.addEventListener('DOMContentLoaded', () => {
         let match = null;
         for (let i = 0; i < lis.length; i++) {
             if (button.textContent === lis[i].textContent) {
-                lis[i].className = 'show';
+                lis[i].className += ' show';
                 match = button.textContent;
             } 
         }
         return match;
     }
 
+    let i = 0;
     qwerty.addEventListener('click', (e) => {
         if (e.target.tagName === 'BUTTON') {
             const button = e.target;
             if (button.className !== 'chosen') {
                 button.className = 'chosen';
                 const checkingLetter = checkLetter(button);
+
                 if (checkingLetter === null) {
-                    const li = ol.children[0];
-                    ol.removeChild(li);
+                    const li = ol.children[i];
+                    i += 1;
+                    const img = li.firstElementChild;
+                    img.src = "images/lostHeart.png";
                     missedGuess += 1;
                 }
+                checkWin();
             }
         }
     });
+
+    function checkWin() {
+        const letterLi = document.getElementsByClassName('letter');
+        const showLi = document.getElementsByClassName('show');
+        console.log('hello', letterLi.length);
+        console.log('bye', showLi.length);
+        
+        if (letterLi.length === showLi.length) {
+            overlay.className = 'win';
+            h2.textContent = 'You Won!';
+
+            overlay.style.display = 'flex';
+        } else if (missedGuess > 4) {
+            overlay.className = 'lose';
+            h2.textContent = 'You Lost!';
+            overlay.style.display = 'flex';
+        }
+    }
 });
